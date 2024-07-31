e252975dff8e868e515cbb2110821dba70ad142d
Kseniya Kabanova <Ksusha1234567810@gmail.com>
2024-08-01 00:32:09 +0430

    add fixes in utils and views

--- src/utils.py
+++ src/utils.py
@@ -29,20 +29,25 @@ logger = setting_log("utils")
 def read_xls_file(file_path: str) -> pd.DataFrame:
     """Функция преобразования Excel-файла в DataFrame"""
     df = pd.read_excel(file_path)
+    logger.info("Файл успешно открыт")
     return df
-
 
 
 def greetings() -> str:
     """Функция приветствия по времени суток"""
     today = datetime.now()
+    logger.info("Функция начала свою работу.")
     if 5 <= today.hour < 12:
+        logger.info("Функция успешно завершила свою работу.")
         return "Доброе утро"
     elif 12 <= today.hour < 17:
+        logger.info("Функция успешно завершила свою работу.")
         return "Добрый день"
     elif 17 <= today.hour < 23:
+        logger.info("Функция успешно завершила свою работу.")
         return "Добрый вечер"
     else:
+        logger.info("Функция успешно завершила свою работу.")
         return "Доброй ночи"
 
 
@@ -50,31 +55,33 @@ def filter_transactions_by_date(transactions: pd.DataFrame, end_date: str = None
     """Функция, фильтрующая данные транзакций по дате.
     Вводимый формат даты: %d.%m.%Y %H:%M:%S.
     Возвращает DataFrame с отфильтрованными транзакциями."""
-
+    logger.info("Функция начала свою работу.")
     if end_date is None:
         end_date_time = datetime.now()
     else:
         try:
             end_date_time = datetime.strptime(end_date, "%d.%m.%Y %H:%M:%S")
         except ValueError:
-            print("Введена некорректная дата. Будет использована текущая дата")
+            logger.error("Введена некорректная дата. Будет использована текущая дата")
             end_date_time = datetime.now()
 
     start_date = end_date_time.replace(day=1)
 
     # Преобразуем столбец 'Дата операции' в формат datetime
-    transactions['Дата операции'] = pd.to_datetime(transactions['Дата операции'], format="%d.%m.%Y %H:%M:%S")
-
+    transactions["Дата операции"] = pd.to_datetime(transactions["Дата операции"], format="%d.%m.%Y %H:%M:%S")
+    logger.info("Функция обрабатывает данные транзакций.")
     # Фильтруем транзакции по дате
-    filtered_transactions = transactions[(transactions['Дата операции'] >= start_date) &
-                                         (transactions['Дата операции'] <= end_date_time)]
-
+    filtered_transactions = transactions[
+        (transactions["Дата операции"] >= start_date) & (transactions["Дата операции"] <= end_date_time)
+    ]
+    logger.info("Функция успешно завершила свою работу.")
     return filtered_transactions.reset_index(drop=True)
 
 
 def filter_transactions_by_card(df_transactions: pd.DataFrame) -> list[dict]:
     """Функция, фильтрующая информацию об операциях по номеру карты
     и выводящая общую информацию по каждой карте в словаре"""
+    logger.info("Функция начала свою работу.")
     cards_dict = (
         df_transactions.loc[df_transactions["Сумма платежа"] < 0]
         .groupby(by="Номер карты")
@@ -82,20 +89,22 @@ def filter_transactions_by_card(df_transactions: pd.DataFrame) -> list[dict]:
         .sum()
         .to_dict()
     )
+    logger.info("Функция обрабатывает данные транзакций.")
     expenses_cards = []
     for card, expenses in cards_dict.items():
         expenses_cards.append(
             {"last_digits": card[-4:], "total_spent": abs(expenses), "cashback": abs(round(expenses / 100, 2))}
         )
+    logger.info("Функция успешно завершила свою работу.")
     return expenses_cards
 
 
 def get_top_five_transactions(filtered_transactions: pd.DataFrame) -> list[dict]:
     """Функция, выдающая информацию о ТОП-5 транзакциях по сумме платежа."""
-
+    logger.info("Функция начала свою работу.")
     # Сортируем транзакции по сумме и выбираем топ-5
-    top_5_transactions = filtered_transactions.nlargest(5, 'Сумма операции')
-
+    top_5_transactions = filtered_transactions.nlargest(5, "Сумма операции")
+    logger.info("Функция обрабатывает данные транзакций.")
     # Создаем список словарей с нужными данными
     top_list = []
     for _, transaction in top_5_transactions.iterrows():
@@ -106,59 +115,56 @@ def get_top_five_transactions(filtered_transactions: pd.DataFrame) -> list[dict]
             "description": transaction["Описание"],
         }
         top_list.append(transaction_dict)
-
+    logger.info("Функция успешно завершила свою работу.")
     return top_list
 
 
 def fetch_exchange_rates(currencies: list) -> dict:
     """Функция, получающая курсы валют"""
+    logger.info("Функция начала свою работу.")
     api_key = ER_API_KEY
     exchange_rates = []
+    logger.info("Функция обрабатывает данные транзакций.")
     for currency in currencies:
         url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount=1"
         headers = {"apikey": api_key}
         response = requests.get(url, headers=headers)
         data = response.json()
         if "result" in data:
-            exchange_rate_dict = {"currency": currency,
-                              "rate": round(data["result"], 2)
-                              }
+            exchange_rate_dict = {"currency": currency, "rate": round(data["result"], 2)}
             exchange_rates.append(exchange_rate_dict)
         else:
             exchange_rates[currency] = {"rate_to_rub": "N/A", "error": data.get("error", "Unknown error")}
+    logger.info("Функция успешно завершила свою работу.")
     return exchange_rates
 
 
 def fetch_stock_prices(stocks: list) -> dict:
     """Функция, получающая цены на акции"""
-    api_url = "https://www.alphavantage.co/query"
+    logger.info("Функция начала свою работу.")
     api_key = SP_API_KEY
     stock_prices = []
+    logger.info("Функция обрабатывает данные транзакций.")
     for stock in stocks:
-        params = {"function": "TIME_SERIES_DAILY", "symbol": stock, "apikey": api_key}
-        response = requests.get(api_url, params=params)
-        if response.status_code == 200:
-            data = response.json()
-            time_series = data.get("Time Series (Daily)")
-            if time_series:
-                latest_date = list(time_series.keys())[0]
-                stock_price_dict = {
-                    "stock": stock,
-                    "price": time_series[latest_date]["4. close"]
-                }
-                stock_prices.append(stock_price_dict)
+        url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={stock}&apikey={api_key}"
+        response = requests.get(url, timeout=5, allow_redirects=False)
+        result = response.json()
+        logger.info(f"{result}")
+        stock_prices.append({"stock": stock, "price": round(float(result["Global Quote"]["05. price"]), 2)})
+    logger.info("Функция успешно завершила свою работу.")
     return stock_prices
 
 
 if __name__ == "__main__":
     df_transactions = read_xls_file("../data/operations.xls")
-
+    my_list = df_transactions.to_dict(orient="records")
     sortirovka = filter_transactions_by_date(df_transactions)
-    print(get_top_five_transactions(sortirovka))
 
-    #print(fetch_exchange_rates(["USD", "EUR"]))
-    #print(fetch_stock_prices(["AAPL", "AMZN", "GOOGL", "MSFT", "TSLA"]))
-    #fltr = filter_transactions_by_date(my_transactions)
+    # print(get_top_five_transactions(sortirovka))
+
+    # print(fetch_exchange_rates(["USD", "EUR"]))
+    print(fetch_stock_prices(["AAPL", "AMZN", "GOOGL", "MSFT", "TSLA"]))
+    # fltr = filter_transactions_by_date(my_transactions)
     # print(process_transactions(my_transactions, date=datetime.now()))
     # print(filter_transactions_by_card(df_transactions))
-    #print(get_top_five_transactions(fltr))
+    # print(get_top_five_transactions(fltr))
