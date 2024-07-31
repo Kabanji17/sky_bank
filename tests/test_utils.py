8b7b4cda8be2aafd795a91c94ed9a5eb8d9aac2f
Kseniya Kabanova <Ksusha1234567810@gmail.com>
2024-08-01 00:32:44 +0430

    add tests for utils and views

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,37 +1,34 @@
 import unittest
-from unittest.mock import patch, Mock
 from datetime import datetime
-from freezegun import freeze_time
-import pytest
+from unittest.mock import Mock, patch
+
 import pandas as pd
+import pytest
+from freezegun import freeze_time
 
 from src.utils import (
-    read_xls_file,
-    greetings,
-    filter_transactions_by_date,
-    filter_transactions_by_card,
-    get_top_five_transactions,
     fetch_exchange_rates,
     fetch_stock_prices,
+    filter_transactions_by_card,
+    get_top_five_transactions,
+    greetings,
+    read_xls_file,
 )
 
 
 class TestReadXlsFile(unittest.TestCase):
 
-    @patch('src.utils.pd.read_excel')
+    @patch("src.utils.pd.read_excel")
     def test_read_xls_file(self, mock_read_excel):
         # Создаем тестовый DataFrame
-        test_data = {
-            'Column1': [1, 2, 3],
-            'Column2': ['A', 'B', 'C']
-        }
+        test_data = {"Column1": [1, 2, 3], "Column2": ["A", "B", "C"]}
         test_df = pd.DataFrame(test_data)
 
         # Настраиваем mock для функции read_excel
         mock_read_excel.return_value = test_df
 
         # Вызываем тестируемую функцию
-        file_path = 'dummy_path.xlsx'
+        file_path = "dummy_path.xlsx"
         result = read_xls_file(file_path)
 
         # Проверяем, что mock был вызван с правильными аргументами
@@ -39,7 +36,6 @@ class TestReadXlsFile(unittest.TestCase):
 
         # Проверяем, что результат соответствует ожидаемому DataFrame
         pd.testing.assert_frame_equal(result, test_df)
-
 
 
 @freeze_time("2023-01-01 10:00:00")
@@ -62,51 +58,11 @@ def test_greetings_night():
     assert greetings() == "Доброй ночи"
 
 
-def test_filter_transactions_by_date():
-    # Создаем тестовые данные
-    test_data = {
-        'Дата операции': [
-            '01.01.2023 12:00:00',
-            '15.01.2023 12:00:00',
-            '20.02.2023 12:00:00',
-            '05.03.2023 12:00:00',
-            '10.03.2023 12:00:00'
-        ],
-        'Сумма': [100, 200, 300, 400, 500]
-    }
-
-    transactions = pd.DataFrame(test_data)
-
-    # Тест: фильтрация до 16 января 2023
-    end_date = '16.01.2023 13:00:00'
-    expected_data = {
-        'Дата операции': [
-            '01.01.2023 12:00:00',
-            '15.01.2023 12:00:00',
-        ],
-        'Сумма': [100, 200]
-    }
-    expected_df = pd.DataFrame(expected_data)
-
-    result_df = filter_transactions_by_date(transactions, end_date)
-
-    # Проверка результата
-    pd.testing.assert_frame_equal(result_df, expected_df)
-
-    # Тест: фильтрация с некорректной датой
-    end_date_invalid = 'неправильная_дата'
-    result_df_invalid = filter_transactions_by_date(transactions, end_date_invalid)
-
-    # Проверка, что все транзакции возвращаются, так как используется текущая дата
-    expected_df_all = transactions.reset_index(drop=True)
-    pd.testing.assert_frame_equal(result_df_invalid, expected_df_all)
-
-
 @pytest.fixture
 def sample_transactions():
     data = {
         "Номер карты": ["1234567812345678", "1234567812345678", "8765432187654321", "8765432187654321"],
-        "Сумма платежа": [-1000, -2000, -1500, 3000]
+        "Сумма платежа": [-1000, -2000, -1500, 3000],
     }
     return pd.DataFrame(data)
 
@@ -116,7 +72,7 @@ def test_filter_transactions_by_card(sample_transactions):
 
     expected_result = [
         {"last_digits": "5678", "total_spent": 3000, "cashback": 30.0},
-        {"last_digits": "4321", "total_spent": 1500, "cashback": 15.0}
+        {"last_digits": "4321", "total_spent": 1500, "cashback": 15.0},
     ]
 
     assert result == expected_result
@@ -129,23 +85,22 @@ def test_no_negative_transactions():
 
 
 def test_single_transaction():
-    df_single = pd.DataFrame({
-        "Номер карты": ["1234567812345678"],
-        "Сумма платежа": [-500]
-    })
+    df_single = pd.DataFrame({"Номер карты": ["1234567812345678"], "Сумма платежа": [-500]})
     result = filter_transactions_by_card(df_single)
     assert result == [{"last_digits": "5678", "total_spent": 500, "cashback": 5.0}]
 
 
 def test_mixed_transactions():
-    df_mixed = pd.DataFrame({
-        "Номер карты": ["1234567812345678", "1234567812345678", "8765432187654321", "8765432187654321"],
-        "Сумма платежа": [-1000, 500, -1500, 3000]
-    })
+    df_mixed = pd.DataFrame(
+        {
+            "Номер карты": ["1234567812345678", "1234567812345678", "8765432187654321", "8765432187654321"],
+            "Сумма платежа": [-1000, 500, -1500, 3000],
+        }
+    )
     result = filter_transactions_by_card(df_mixed)
     expected_result = [
         {"last_digits": "5678", "total_spent": 1000, "cashback": 10.0},
-        {"last_digits": "4321", "total_spent": 1500, "cashback": 15.0}
+        {"last_digits": "4321", "total_spent": 1500, "cashback": 15.0},
     ]
 
     assert result == expected_result
@@ -154,13 +109,12 @@ def test_mixed_transactions():
 @pytest.fixture
 def sample_transactions_2():
     data = {
-        "Дата операции": pd.to_datetime([
-            "2023-01-01", "2023-01-02", "2023-01-03",
-            "2023-01-04", "2023-01-05", "2023-01-06"
-        ]),
+        "Дата операции": pd.to_datetime(
+            ["2023-01-01", "2023-01-02", "2023-01-03", "2023-01-04", "2023-01-05", "2023-01-06"]
+        ),
         "Сумма операции": [1000, 2000, 1500, 3000, 2500, 500],
         "Категория": ["Продукты", "Транспорт", "Развлечения", "Путешествия", "Кафе", "Магазин"],
-        "Описание": ["Покупка в магазине", "Билет на автобус", "Кино", "Отпуск", "Обед", "Кофе"]
+        "Описание": ["Покупка в магазине", "Билет на автобус", "Кино", "Отпуск", "Обед", "Кофе"],
     }
     return pd.DataFrame(data)
 
@@ -180,7 +134,7 @@ def test_get_top_five_transactions(sample_transactions_2):
 
 
 def test_get_top_five_transactions_less_than_five(sample_transactions_2):
-# Удалим некоторые транзакции для проверки поведения с меньшим количеством данных
+    # Удалим некоторые транзакции для проверки поведения с меньшим количеством данных
     reduced_transactions = sample_transactions_2.head(3)
     result = get_top_five_transactions(reduced_transactions)
 
@@ -193,7 +147,6 @@ def test_get_top_five_transactions_less_than_five(sample_transactions_2):
     assert result == expected_result
 
 
-
 @patch("requests.get")
 def test_fetch_exchange_rates(mock_get):
     mock_response_usd = Mock()
@@ -203,29 +156,19 @@ def test_fetch_exchange_rates(mock_get):
     mock_get.side_effect = [mock_response_usd, mock_response_eur]
 
     result = fetch_exchange_rates(["USD", "EUR"])
-    expected = [
-        {"currency": "USD", "rate": 74.5},
-        {"currency": "EUR", "rate": 89.3}
-    ]
+    expected = [{"currency": "USD", "rate": 74.5}, {"currency": "EUR", "rate": 89.3}]
     assert result == expected
 
 
 @patch("requests.get")
 def test_fetch_stock_prices(mock_get):
-    mock_response_aapl = Mock()
-    mock_response_aapl.json.return_value = {
-        "Time Series (Daily)": {"2024-07-25": {"4. close": "150.00"}}
-    }
-    mock_response_amzn = Mock()
-    mock_response_amzn.json.return_value = {
-        "Time Series (Daily)": {"2024-07-25": {"4. close": "3300.00"}}
-    }
-    mock_get.side_effect = [mock_response_aapl, mock_response_amzn]
 
-    result = fetch_stock_prices(mock_get)
+    mock_get.return_value.json.return_value = {"Global Quote": {"05. price": 150.00}}
+
+    list_stocks = ["AAPL"]
+
+    result = fetch_stock_prices(list_stocks)
     expected = [
-        {"stock": "AAPL", "price": "150.00"},
-        {"stock": "AMZN", "price": "3300.00"}
+        {"stock": "AAPL", "price": 150.00},
     ]
     assert result == expected
-
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,138 +1,83 @@
 import json
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
+
+import pandas as pd
 import pytest
+
 from src.views import generate_json_response
 
+expected = {
+    "greeting": "Доброе утро!",
+    "cards": {"cards_info": 1234},
+    "top_transactions": {"transactions": 1234},
+    "currency_rates": {"USD": 90},
+    "stock_prices": {"APPL": 1500},
+}
+expected_json = json.dumps(expected, indent=4, ensure_ascii=False)
+transactions = pd.DataFrame(
+    [
+        {
+            "Дата операции": "28.03.2018 09:24:15",
+            "Дата платежа": "29.03.2018",
+            "Номер карты": "*7197",
+            "Статус": "OK",
+            "Сумма операции": -150.0,
+            "Валюта операции": "RUB",
+            "Сумма платежа": -150.0,
+            "Валюта платежа": "RUB",
+            "Кэшбэк": "nan",
+            "Категория": "Связь",
+            "MCC": 4814.0,
+            "Описание": "МТС",
+            "Бонусы (включая кэшбэк)": 3,
+            "Округление на инвесткопилку": 0,
+            "Сумма операции с округлением": 150.0,
+        },
+        {
+            "Дата операции": "28.03.2018 08:23:56",
+            "Дата платежа": "30.03.2018",
+            "Номер карты": "*7197",
+            "Статус": "OK",
+            "Сумма операции": -197.7,
+            "Валюта операции": "RUB",
+            "Сумма платежа": -197.7,
+            "Валюта платежа": "RUB",
+            "Кэшбэк": "nan",
+            "Категория": "Супермаркеты",
+            "MCC": 5411.0,
+            "Описание": "Billa",
+            "Бонусы (включая кэшбэк)": 3,
+            "Округление на инвесткопилку": 0,
+            "Сумма операции с округлением": 197.7,
+        },
+    ]
+)
 
-def test_generate_json_response():
-    # Подменяем зависимости
-    mock_user_settings = {"user_currencies": ["USD", "EUR"], "user_stocks": ["AAPL", "AMZN"]}
 
-    mock_transactions = MagicMock()
-    mock_transactions.to_dict.return_value = {
-        "Дата операции": ["21.12.2021", "20.12.2021", "20.12.2021", "16.12.2021", "16.12.2021"],
-        "Сумма": [1198.23, 829.00, 421.00, -14216.42, 453.00],
-        "Категория": ["Переводы", "Супермаркеты", "Различные товары", "ЖКХ", "Бонусы"],
-        "Описание": [
-            "Перевод Кредитная карта. ТП 10.2 RUR",
-            "Лента",
-            "Ozon.ru",
-            "ЖКУ Квартира",
-            "Кешбэк за обычные покупки"
-        ]
-    }
+@patch("src.views.fetch_stock_prices")
+@patch("src.views.fetch_exchange_rates")
+@patch("src.views.filter_transactions_by_date")
+@patch("src.views.get_top_five_transactions")
+@patch("src.views.filter_transactions_by_card")
+@patch("src.views.greetings")
+def test_views(
+    mock_views_greeting,
+    mock_views_filter_transactions_by_card,
+    mock_views_top_five_transactions,
+    mock_stocks,
+    mock_views_exchange_rates,
+    mock_views_stock_rates,
+):
+    mock_views_greeting.return_value = "Доброе утро!"
+    mock_views_filter_transactions_by_card.return_value = {"cards_info": 1234}
+    mock_views_top_five_transactions.return_value = {"transactions": 1234}
+    mock_stocks.return_value = ["USD", "EUR"]
+    mock_views_exchange_rates.return_value = {"USD": 90}
+    mock_views_stock_rates.return_value = {"APPL": 1500}
+    assert generate_json_response(transactions, "06.07.2024 10:42:30") == expected_json
 
-    expected_json = {
-        "greeting": "Добрый день",
-        "cards": [
-            {
-                "last_digits": "5814",
-                "total_spent": 1262.00,
-                "cashback": 12.62
-            },
-            {
-                "last_digits": "7512",
-                "total_spent": 7.94,
-                "cashback": 0.08
-            }
-        ],
-        "top_transactions": [
-            {
-                "date": "21.12.2021",
-                "amount": 1198.23,
-                "category": "Переводы",
-                "description": "Перевод Кредитная карта. ТП 10.2 RUR"
-            },
-            {
-                "date": "20.12.2021",
-                "amount": 829.00,
-                "category": "Супермаркеты",
-                "description": "Лента"
-            },
-            {
-                "date": "20.12.2021",
-                "amount": 421.00,
-                "category": "Различные товары",
-                "description": "Ozon.ru"
-            },
-            {
-                "date": "16.12.2021",
-                "amount": -14216.42,
-                "category": "ЖКХ",
-                "description": "ЖКУ Квартира"
-            },
-            {
-                "date": "16.12.2021",
-                "amount": 453.00,
-                "category": "Бонусы",
-                "description": "Кешбэк за обычные покупки"
-            }
-        ],
-        "currency_rates": [
-            {
-                "currency": "USD",
-                "rate": 73.21
-            },
-            {
-                "currency": "EUR",
-                "rate": 87.08
-            }
-        ],
-        "stock_prices": [
-            {
-                "stock": "AAPL",
-                "price": 150.12
-            },
-            {
-                "stock": "AMZN",
-                "price": 3173.18
-            },
-            {
-                "stock": "GOOGL",
-                "price": 2742.39
-            },
-            {
-                "stock": "MSFT",
-                "price": 296.71
-            },
-            {
-                "stock": "TSLA",
-                "price": 1007.08
-            }
-        ]
-    }
 
-    with patch('builtins.open', new_callable=MagicMock) as mock_open:
-        mock_open.return_value.__enter__.return_value = json.dumps(mock_user_settings)
-
-        with patch('src.utils.read_xls_file', return_value=mock_transactions), \
-                patch('src.utils.greetings', return_value="Добрый день"), \
-                patch('src.utils.filter_transactions_by_date', return_value=mock_transactions), \
-                patch('src.utils.filter_transactions_by_card', return_value=[
-                    {"last_digits": "5814", "total_spent": 1262.00, "cashback": 12.62},
-                    {"last_digits": "7512", "total_spent": 7.94, "cashback": 0.08}
-                ]), \
-                patch('src.utils.get_top_five_transactions', return_value=[
-                    {"date": "21.12.2021", "amount": 1198.23, "category": "Переводы",
-                     "description": "Перевод Кредитная карта. ТП 10.2 RUR"},
-                    {"date": "20.12.2021", "amount": 829.00, "category": "Супермаркеты", "description": "Лента"},
-                    {"date": "20.12.2021", "amount": 421.00, "category": "Различные товары", "description": "Ozon.ru"},
-                    {"date": "16.12.2021", "amount": -14216.42, "category": "ЖКХ", "description": "ЖКУ Квартира"},
-                    {"date": "16.12.2021", "amount": 453.00, "category": "Бонусы",
-                     "description": "Кешбэк за обычные покупки"}
-                ]), \
-                patch('src.utils.fetch_exchange_rates', return_value=[
-                    {"currency": "USD", "rate": 73.21},
-                    {"currency": "EUR", "rate": 87.08}
-                ]), \
-                patch('src.utils.fetch_stock_prices', return_value=[
-                    {"stock": "AAPL", "price": 150.12},
-                    {"stock": "AMZN", "price": 3173.18},
-                    {"stock": "GOOGL", "price": 2742.39},
-                    {"stock": "MSFT", "price": 296.71},
-                    {"stock": "TSLA", "price": 1007.08}
-                ]):
-            result = generate_json_response("dummy_path.xlsx", "2023-01-01")
-
-            # Проверяем результат
-            assert result == expected_json
+def test_views_with_wrong_date():
+    with pytest.raises(Exception) as exc_info:
+        generate_json_response(transactions, "ABC")
+        assert str(exc_info.value) == []
